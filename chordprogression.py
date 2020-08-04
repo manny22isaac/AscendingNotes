3fa24889a935d5fe7678fcc5b19753f8afcd3622
manny22isaac <34252065+manny22isaac@users.noreply.github.com>
2020-08-03 20:49:07 -0400

    Extended Main & Added Scale/Mode Selector
    - Added code within main function 
    - Added Modern & Exotic scales in dictionary format
    - Removed scale variable outside class

--- chordprogression.py
+++ chordprogression.py
@@ -1,18 +1,28 @@
 
 import random
-#musicalModes
-
 notes = ['C', 'C#', 'D', 'D#',
              'E', 'F', 'F#', 'G', 'G#',
              'A', 'A#','B']
 
+scale = {'major':[0, 2, 4, 5, 7, 9, 11],
+    'minor':[0, 2, 3, 5, 7, 9, 10],
+    'phrygian':[0, 1, 3, 5, 7, 8, 10],
+    'lydian':[0, 2, 4, 6, 7, 9, 11],
+    'mixolydian':[0, 2, 4, 5, 7, 9, 10],
+    'aeolian':[0, 2, 3, 5, 7, 8, 10],
+    'locrian':[0, 1, 3, 5, 6, 8, 10],
+    'chromatic':[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    'wholetone':[0, 2, 4, 6, 8, 10],
+    'diminished7thchord': [0, 3, 6, 9],
+    'augmentedchord':[0, 4, 8],
+    'tritone':[0, 6]}
+
+
 rootkey = input("Enter the root Note: ").upper()
-scale = input("Enter the Mode: ").lower()
 
 class CircleFifths:
     """docstring for CircleFifths."""
-    def __init__(self, scale, key):
-        self.scale = scale
+    def __init__(self, key):
         self.rootkey = rootkey
 
     def shift(self, rootkey, index=0, s=0,):
@@ -26,22 +36,33 @@ class CircleFifths:
                 print(new_notes)
                 return new_notes
 
-    #def modes(self, rootkey):
+    #def modes(self, new_notes):
         #this function is currently being fixed
         #this takes the indexes of a string list of notes
         # and matches them to the integers of another list represented as a scale
         #keymode = []
+
         #major = [0, 2, 4, 5, 7, 9, 11]
         #keymode = [new_notes[i] for i in major]
         #print(keymode)
 
-a = CircleFifths(scale, rootkey)
-a.shift(rootkey)
-
 
 def main():
-    a = CircleFifths(scale, rootkey)
-    a.shift(rootkey)
-    #a.modes(scale)
+    a = CircleFifths(rootkey)
+    key = a.shift(rootkey)
+
+    keymode = []
+
+    print('Select a scale and enter it below')
+    for k in scale:
+        print(k)
+
+    mode = input("Enter the mode: ").lower()
+    x = scale.get(mode)
+
+
+    keymode = [key[i] for i in x]
+    print(keymode)
+
 
 main()
